c41f77dfaa580d828109bb7b0e42e1ee46cc8ed9
kusa104 <101918225+kusa104@users.noreply.github.com>
2022-05-16 21:17:22 +0900

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyCe8pz5rB_S4CXDmw2ofLlaXnRj1jmlCkA",
-  authDomain: "vue-calendar-f1cfa.firebaseapp.com",
-  projectId: "vue-calendar-f1cfa",
-  storageBucket: "vue-calendar-f1cfa.appspot.com",
-  messagingSenderId: "104731286019",
-  appId: "1:104731286019:web:84d95911ffd8d66ce78c36"
-}
